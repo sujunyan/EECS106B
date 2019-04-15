2464e9ce5af2762babe8d6f498dc7b3b07780a3a
Junyan Su <sujy5011@gmail.com>
2019-04-14 20:05:54 -0700

    working on PointCloudAnalyzer

--- proj/src/point_cloud_analyzer/inc/PointCloudAnalyzer.hpp
+++ proj/src/point_cloud_analyzer/inc/PointCloudAnalyzer.hpp
@@ -7,8 +7,19 @@ private:
 public:
     PointCloudAnalyzer(/* args */);
     ~PointCloudAnalyzer();
+    void start();
 private:
-    ros::Publisher  m_pubGray;
+    void setup_parameter();
+    void callback(const PointCloud::ConstPtr& msg)
+
+    ros::NodeHandle nh;
+    ros::Publisher use_case_pub;
+	ros::Publisher max_filter_pub;
+	ros::Publisher min_filter_pub;
+	ros::Publisher exposure_pub;
+    
+    ros::Subscriber point_cloud_sub;
+
 
 };
 
--- proj/src/point_cloud_analyzer/src/PointCloudAnalyzer.cpp
+++ proj/src/point_cloud_analyzer/src/PointCloudAnalyzer.cpp
@@ -1,4 +1,7 @@
 #include "PointCloudAnalyzer.hpp"
+#include "std_msgs/String.h"
+#include "std_msgs/Float32.h"
+#include "std_msgs/UInt32.h"
 
 PointCloudAnalyzer::PointCloudAnalyzer(/* args */)
 {
@@ -6,4 +9,41 @@ PointCloudAnalyzer::PointCloudAnalyzer(/* args */)
 
 PointCloudAnalyzer::~PointCloudAnalyzer()
 {
+    setup_parameter();
 }
+
+void PointCloudAnalyzer::setup_parameter(){
+    use_case_pub = nh.advertise<std_msgs::String>("/use_case", 1);
+    max_filter_pub = nh.advertise<std_msgs::Float32>("/max_filter", 1);
+    min_filter_pub = nh.advertise<std_msgs::Float32>("/min_filter", 1);
+    exposure_pub = nh.advertise<std_msgs::UInt32>("/expo_time", 1);
+	sleep(3);
+
+	std_msgs::String mode_msg("MODE_5_45FPS_500");
+	use_case_pub.publish(mode_msg);
+
+    std_msgs::Float32 max_filter_msg;
+	max_filter_msg.data = 0.3;
+	max_filter_pub.publish(max_filter_msg);
+
+	std_msgs::Float32 min_filter_msg;
+	min_filter_msg.data = 0.06;
+	min_filter_pub.publish(min_filter_msg);
+
+	std_msgs::UInt32 expo_time_msg;
+	expo_time_msg.data = 50;
+	exposure_pub.publish(expo_time_msg);
+	sleep(3);
+
+	ros::spinOnce();	
+}
+
+void PointCloundAnalyzer::start(){
+	point_cloud_sub = nh.subscribe<PointCloud>("/royale_camera_driver/point_cloud", 1, callback);
+}
+
+void PointCloudAnalyzer::callback(const PointCloud::ConstPtr& msg)
+{
+
+}
+
